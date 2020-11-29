7ec5134ae0b54463df3d1f1e6832403a1dea471e
TibRib <meta-block@hotmail.fr>
2020-11-29 23:37:27 +0100

    HttpClient Marvel API headers

--- hero-polls/src/app/perso.service.ts
+++ hero-polls/src/app/perso.service.ts
@@ -1,24 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+
+/* MARVEL API 
+  CALLS PER DAY : 3000
+  URL : http://gateway.marvel.com/
+  docs : https://developer.marvel.com/docs
+  */
+
+const MARVEL_URL = "http://gateway.marvel.com";
+const MARVEL_PUB_KEY= "d6e02dd7c891815142fcac32c5c10859";
+const MARVEL_PRIV_KEY = "7124daa3a6fc215551ec2e84c5127989333906ef";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersoService {
-  token : string = "MY_TOKEN"
+  marvel_page : number = 0
 
   constructor(private http: HttpClient) { }
 
-  
-  getPersos(): void{
+  private marvelHeaders() : any
+  {
+    var md5 = require('md5');
+    var ts = String(Date.now());
     const headers = { 
-      Authorization: `Bearer ${this.token}`
+      apikey: MARVEL_PUB_KEY,
+      ts: ts,
+      hash: String(md5(ts+MARVEL_PRIV_KEY+MARVEL_PUB_KEY)), //Hash = md5 ( ts+privateKey+publicKey )
+      limit : String(24),
+      offset : String(this.marvel_page*24),
     };
 
-    this.http.get('http://url', { headers })
-      // will call the URL http://url?sort=ascending&page=1
+    return headers;
+  }
+  
+  getPersosMARVEL(): void{
+    const headers=  this.marvelHeaders()
+    this.http.get(MARVEL_URL+"/v1/public/characters", { headers })
       .subscribe(response => { 
         //this.myData = response; 
+        console.log(response);
+      });
+  }
+
+  getPersoMARVEL(id : number){
+    const headers=  this.marvelHeaders()
+    this.http.get(MARVEL_URL+"/v1/public/characters/"+id, { headers })
+      .subscribe(response => { 
+        //this.myData = response;
         console.log(response);
       });
   }
