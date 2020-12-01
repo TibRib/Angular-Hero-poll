de6160fb05fa56ab8266a51a37a0021df6fdb241
TibRib <meta-block@hotmail.fr>
2020-12-01 19:23:12 +0100

    Proxy CORS configuration for marvel API forwarding

--- hero-polls/src/app/perso.service.ts
+++ hero-polls/src/app/perso.service.ts
@@ -4,11 +4,11 @@ import {Md5} from 'ts-md5/dist/md5';
 
 /* MARVEL API 
   CALLS PER DAY : 3000
-  URL : http://gateway.marvel.com/
+  URL : http://gateway.marvel.com
   docs : https://developer.marvel.com/docs
   */
 
-const MARVEL_URL = "http://gateway.marvel.com";
+const MARVEL_URL = "marvel";
 const MARVEL_PUB_KEY= "d6e02dd7c891815142fcac32c5c10859";
 const MARVEL_PRIV_KEY = "7124daa3a6fc215551ec2e84c5127989333906ef";
 
@@ -25,11 +25,11 @@ export class PersoService {
     
     var ts = String(Date.now());
     const headers = { 
-      apikey: MARVEL_PUB_KEY,
-      ts: ts,
-      hash: String(  Md5.hashStr(ts+MARVEL_PRIV_KEY+MARVEL_PUB_KEY) ), //Hash = md5 ( ts+privateKey+publicKey )
-      limit : String(24),
-      offset : String(this.marvel_page*24),
+      "apikey": MARVEL_PUB_KEY,
+      "ts": ts,
+      "hash": String(  Md5.hashStr(ts+MARVEL_PRIV_KEY+MARVEL_PUB_KEY) ), //Hash = md5 ( ts+privateKey+publicKey )
+      "limit" : String(24),
+      "offset" : String(this.marvel_page*24),
     };
 
     return headers;
@@ -38,13 +38,19 @@ export class PersoService {
   getPersosMARVEL(): void{
     const headers=  this.marvelHeaders()
     console.log(headers);
-    
-    this.http.get(MARVEL_URL+"/v1/public/characters", { headers })
+
+    this.http.get("/marvel/v1/public/characters?apikey=d6e02dd7c891815142fcac32c5c10859&ts=1606846806128&hash=e7c9553d739815193473c401c344c9f6&limit=24&offset=0")
       .subscribe(response => { 
         //this.myData = response; 
         console.log(response);
       });
-      
+    /*
+    this.http.get("/marvel/v1/public/characters", { headers })
+      .subscribe(response => { 
+        //this.myData = response; 
+        console.log(response);
+      });
+      */
   }
 
   getPersoMARVEL(id : number){
