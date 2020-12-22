aa7584c94c05c1d5aed1f9a3bf19c63463858bf9
TibRib <meta-block@hotmail.fr>
2020-12-23 00:40:36 +0100

    Commentaires all-hero-page component

--- hero-polls/src/app/all-heroes-page/all-heroes-page.component.ts
+++ hero-polls/src/app/all-heroes-page/all-heroes-page.component.ts
@@ -21,6 +21,7 @@ import { PersoService } from '../perso.service';
   <hr>
     
     <hp-hero-list [heros]="heros"></hp-hero-list>
+    
     <div class="mx-auto text-center">
       <button type="button" (click)="setPage(page-1)" *ngIf="(page-1)>0" class="btn btn-primary"><{{page-1}}</button>
       <button type="button" class="btn btn-info" disabled>{{page}}</button>
@@ -41,16 +42,21 @@ export class AllHeroesPageComponent implements OnInit {
     this.route.paramMap.subscribe((params: ParamMap) => {
        this.page = +this.route.snapshot.paramMap.get('page');
        if(this.page < 1){
+         //On borne le numéro de page à minimum 1.
         this.setPage(1);
        }
     });
+    //On déclenche l'appel du contenu
     this.fetchHeroes();
   }
 
+  /* Méthode de récupération des héros à afficher, pour la page X décalée sur l'intervalle [0:n] au lieu de [1:n+1] */
   fetchHeroes(): void{
+    //Récupération asynchrone de la liste des héros à afficher
     this.heroService.getPersosMARVEL(this.page-1).subscribe((r)=> this.heros=r);
   }
 
+  /* Change la page ( ex: /5 ) dans le routing et actualise le contenu */
   setPage(value){
     this.page = value;
     console.log("Page updated to "+this.page);
