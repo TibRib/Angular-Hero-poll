6477dec33f670f90faf9f6de3406380add6ee3fe
TibRib <meta-block@hotmail.fr>
2020-12-23 00:50:40 +0100

    Commentaires hero-details component

--- hero-polls/src/app/hero-details/hero-details.component.ts
+++ hero-polls/src/app/hero-details/hero-details.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Perso } from '../perso';
 
+/* Ce composant est représentatif du contenu associé
+ * A la page de détails d'un héros.
+ * On y trouvera ainsi
+ * - La description
+ * - Les aptitudes, s'il y a lieu
+ * - Les connexions/Relations = personnage en lien, s'il y a lieu
+ * du personnage associé par Input
+ * */
+
 @Component({
   selector: 'hp-hero-details',
   templateUrl: 'details_template.html',
